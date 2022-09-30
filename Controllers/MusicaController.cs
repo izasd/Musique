120e20f8d0c1e15fe849b04730b6f1324eae16c0
giselleasv <giselleasv@gmail.com>
2022-09-30 15:57:06 -0300

    adição crud musica

--- Controllers/MusicaController.cs
+++ Controllers/MusicaController.cs
@@ -18,4 +18,62 @@ public class MusicaController : Controller
         var musicas = _db.Musicas.ToList();
         return View(musicas);
     }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        var musica = new Musica();
+        return View(musica);
+    }
+    [HttpPost]
+    public IActionResult Create(Musica musica)
+    {
+        if(!ModelState.IsValid) return View(musica);
+        _db.Musicas.Add(musica);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var musica = _db.Musicas.Find(id);
+        if(musica is null)
+            return RedirectToAction("Index");
+        return View(musica);
+    }
+    [HttpPost]
+    public IActionResult Edit(int id, Musica musica)
+    {
+        var originalmusica = _db.Musicas.Find(id);
+        if(originalmusica is null)
+           return RedirectToAction("Index");
+        if(!ModelState.IsValid){
+            return View(musica);
+        }            
+        
+        originalmusica.Nome = musica.Nome;
+        originalmusica.Letra = musica.Letra;        
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var musica = _db.Musicas.Find(id);
+        if(musica is null)
+            return RedirectToAction("Index");
+        return View(musica);
+    }
+    [HttpPost]
+    public IActionResult ProcessDelete(int IdMusica)
+    {
+        var musica = _db.Musicas.Find(IdMusica);
+        if(musica is null)
+            return RedirectToAction("Index");
+        _db.Musicas.Remove(musica);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
 }
